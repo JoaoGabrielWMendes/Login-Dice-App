2262fd52a5a1336335ee28307077a7fdb94db412
João GWM <joaogwmendes@gmail.com>
2025-07-11 11:53:42 -0300

    Refactor variables, extract helpers, and add on_close for safe shutdown

--- teste.py
+++ teste.py
@@ -1,89 +1,78 @@
 import customtkinter as ctk
 import sqlite3
-from PIL import Image, ImageTk
-window = ctk.CTk()
+from tkinter import PhotoImage
+from PIL import Image
+from customtkinter import CTkImage
+from assets.functions import entry_fields, clear
+app = ctk.CTk()
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("dark-blue")
-image=Image.open("assets/success.png")
-image_resize=image.resize((200,200))
-window.iconbitmap("assets/icon.ico")
-image_tk=ImageTk.PhotoImage(image_resize)
-window.title("Login test")
-window.geometry("500x500")
-window.resizable(False, False)
-con=sqlite3.connect("db.db")
-cur = con.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS db (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL UNIQUE CHECK (username <> ''), password TEXT NOT NULL CHECK (password <> ''))")
-con.commit()
-cur.execute
-def main():
-    clear()
+success_img_PIL=Image.open("assets/success.png")
+success_img = CTkImage(dark_image=success_img_PIL, size=(200, 200))
+app.title("Login test")
+app.geometry("500x500")
+app.resizable(False, False)
+conn=sqlite3.connect("users.db")
+cursor = conn.cursor()
+cursor.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL UNIQUE CHECK (username <> ''), password TEXT NOT NULL CHECK (password <> ''))")
+conn.commit()
+cursor.execute
+def show_login_screen():
+    clear(app)
     login()
 def login():
-    label = ctk.CTkLabel(window, text="Login",font=("", 25))
-    label.pack(pady=10)
-    username= ctk.CTkLabel(window, text="Username")
-    username.pack()
-    username_entry=ctk.CTkEntry(window)
-    username_entry.pack()
-    password = ctk.CTkLabel(window,text="Password")
-    password.pack()
-    password_entry=ctk.CTkEntry(window, show="*")
-    password_entry.pack()
-    button = ctk.CTkButton(window, text="Login", command=lambda: [check_login()])
-    button.pack(pady=(15,5))
-    button_account=ctk.CTkButton(window,text="Create Account",fg_color="transparent",text_color="#1f538d",command=lambda: [clear(), create_account()])
-    button_account.pack(pady=(2,0))
-    label_error = ctk.CTkLabel(window, text="")
+    login_label_title = ctk.CTkLabel(app, text="Login",font=("", 25))
+    login_label_title.pack(pady=10)
+    login_username_entry=entry_fields("Username", app)
+    login_password_entry=entry_fields("Password", app)
+    login_button_submit = ctk.CTkButton(app, text="Login", command=lambda: [check_login()])
+    login_button_submit.pack(pady=(15,5))
+    create_account_button=ctk.CTkButton(app,text="Create Account",fg_color="transparent",text_color="#1f538d",command=lambda: [clear(app), create_account()])
+    create_account_button.pack(pady=(2,0))
+    label_error = ctk.CTkLabel(app, text="")
     label_error.pack()
     def check_login():
-        cur.execute("SELECT 1 FROM db WHERE username=? AND password=?",(username_entry.get(),password_entry.get()))
-        if cur.fetchone():
+        cursor.execute("SELECT 1 FROM users WHERE username=? AND password=?", (login_username_entry.get(), login_password_entry.get()))
+        if cursor.fetchone():
             return login_success()
         else:
-            label_error.configure(text="Invalid username or password")
+            label_error.configure(text="Invalid username or password.")
 def login_success():
-    clear()
-    label = ctk.CTkLabel(window, text="Login Successful", font=("", 25))
-    label.pack(pady=10)
-    image_label=ctk.CTkLabel(window,image=image_tk, text="")
-    image_label.pack()
-    button_back=ctk.CTkButton(window, text="Back",fg_color="transparent",text_color="#1f538d", font=("", 14), command=lambda: [clear(), main()])
-    button_back.pack(pady=10)
+    clear(app)
+    success_label_title = ctk.CTkLabel(app, text="Login Successful", font=("", 25))
+    success_label_title.pack(pady=10)
+    success_img_label=ctk.CTkLabel(app,image=success_img, text="")
+    success_img_label.pack()
+    button_back_login=ctk.CTkButton(app, text="Back",fg_color="transparent",text_color="#1f538d", font=("", 14), command=lambda: [clear(app), show_login_screen()])
+    button_back_login.pack(pady=10)
 def create_account():
-    label=ctk.CTkLabel(window,text="Create Account",font=("", 25))
-    label.pack(pady=10)
-    username=ctk.CTkLabel(window,text="Username")
-    username.pack()
-    username_entry=ctk.CTkEntry(window)
-    username_entry.pack()
-    password=ctk.CTkLabel(window,text="Password")
-    password.pack()
-    password_entry=ctk.CTkEntry(window)
-    password_entry.pack()
-    button=ctk.CTkButton(window,text="Create Account",command=lambda: [insert_db()])
-    button.pack(pady=(15,5))
-    button_back=ctk.CTkButton(window,text="Back",fg_color="transparent",text_color="#1f538d",command=main)
-    button_back.pack(pady=(2,0))
-    error_label = ctk.CTkLabel(window, text="")
+    create_account_label_title=ctk.CTkLabel(app,text="Create Account",font=("", 25))
+    create_account_label_title.pack(pady=10)
+    create_account_username_entry=entry_fields("Username", app)
+    create_account_password_entry=entry_fields(" Password ", app)
+    create_account_button_submit=ctk.CTkButton(app,text="Create Account",command=lambda: [insert_user()])
+    create_account_button_submit.pack(pady=(15,5))
+    button_back_login=ctk.CTkButton(app,text="Back",fg_color="transparent",text_color="#1f538d",command=show_login_screen)
+    button_back_login.pack(pady=(2,0))
+    error_label = ctk.CTkLabel(app, text="")
     error_label.pack()
-    def insert_db():
+    def insert_user():
         try:
-            if not username_entry.get().strip() or not password_entry.get().strip():
+            if not create_account_username_entry.get().strip() or not create_account_password_entry.get().strip():
                 error_label.configure(text="Username or password cannot be empty.")
             else:
-                cur.execute("INSERT INTO db (username, password) VALUES(?,?)", (username_entry.get(),password_entry.get()))
-                con.commit()
-                print(f"Account {username_entry.get()} and {password_entry.get()} created")
+                cursor.execute("INSERT INTO users (username, password) VALUES(?,?)", (create_account_username_entry.get(), create_account_password_entry.get()))
+                conn.commit()
                 error_label.configure(text="")
-                return main()
+                return show_login_screen()
         except sqlite3.IntegrityError as e:
             if 'CHECK constraint failed' in str(e):
                 error_label.configure(text="Username or password cannot be empty.")
             else:
                 error_label.configure(text="Username already exists.")
-def clear():
-    for widget in window.winfo_children():
-        widget.destroy()
-main()
-window.mainloop()
+show_login_screen()
+def on_close():
+    conn.close()
+    app.destroy()
+app.protocol("WM_DELETE_WINDOW", on_close)
+app.mainloop()
